087f6e3f819793c832eae929f498bd3c9ed04ef0
Mingwang Huo <90225549+huo1m1w1@users.noreply.github.com>
2023-02-08 03:13:52 +0000

    Update kafka_producer.py

--- batch_process/save_data_to_S3_by_kafka/kafka_producer.py
+++ batch_process/save_data_to_S3_by_kafka/kafka_producer.py
@@ -15,13 +15,17 @@ from kafka.errors import (UnknownError, KafkaConnectionError, FailedPayloadsErro
 
 class Kafka_producer:
     """
-    This class w=is with functions of creating topics, and sending data to kafka, 
-    also with function of receiving data from original source (in user_posting_emulation file) which is blocked in this project.
+    This class with functions of creating topics, and sending data to kafka, 
+    also with function of receiving data from original source (in user_posting_emulation file).
     """
     def __init__(self) -> None:
         pass
 
     def create_broker(self, topic_name, n_partitions, n_replication_factors):
+      
+        """
+        Create Kafka broker/s
+        """
 
         admin_client = KafkaAdminClient(
             bootstrap_servers="localhost:9092",
@@ -37,34 +41,55 @@ class Kafka_producer:
         )
         admin_client.create_topics(new_topics=topic_list, validate_only=False)
 
-    # initialise kafka producer
+    
     def producer(self):
+      
+        """
+        initialise kafka producer
+        """
         self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
               api_version=(3,2,1),
               value_serializer=lambda x: json.dumps(x).encode('utf-8'))
     
     def create_engine(self):
+      
+        """
+        Create an engine to read data from source
+        """
         new_connector = AWSDBConnector()
         self.engine = new_connector.create_db_connector()
 
 
     def get_origenal_data(self,nth_pin):
-
+        
+        """
+        Read data from source
+        """
         row = self.engine.execute(f"SELECT * FROM pinterest_data LIMIT {nth_pin}, 1")
         message = dict(row.mappings().all()[0])
         return message
 
-    # sending data to kafka, message is expected a json file
+     
     def sending_data_to_kafka(self, topic_name, message):
-
+        
+        """
+        sending data to kafka, message is expected a json object
+        """
         self.producer.send(topic_name, message)
 
     def list_of_topics():
-        # list all topics
+         
+        """
+        list all topics in given kafka broker
+        """
         consumer = KafkaConsumer( bootstrap_servers=['localhost:9092'])
         consumer.topics()
 
     def delete_topics(topic_names):
+      
+        """
+        delete topic/s from given kafka topic/s
+        """
         admin_client = KafkaAdminClient(bootstrap_servers=['localhost:9092'])
         try:
             admin_client.delete_topics(topics=topic_names)
